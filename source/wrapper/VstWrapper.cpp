b9d1eede75f841531e58b84929cc4a25947c5f9b
Nik Reiman <teragonaudio@mac.com>
2010-02-17 16:14:18 +0100

    Defined i/o properties

--- source/wrapper/VstWrapper.cpp
+++ source/wrapper/VstWrapper.cpp
@@ -1,10 +1,18 @@
+#ifndef __VstWrapper_h__
 #include "VstWrapper.h"
+#endif
 
 AudioEffect* createEffectInstance(audioMasterCallback audioMaster) {
 	return new VstWrapper(audioMaster);
 }
 
 VstWrapper::VstWrapper(audioMasterCallback audioMaster) : AudioEffectX(audioMaster, 0, NUM_PARAMS) {
+  if(audioMaster) {
+    setNumInputs(NUM_INPUTS);
+    setNumOutputs(NUM_OUTPUTS);
+    setUniqueID(UNIQUE_ID);
+    canProcessReplacing();
+  }
 }
 
 VstWrapper::~VstWrapper() {
@@ -12,3 +20,4 @@ VstWrapper::~VstWrapper() {
 
 void VstWrapper::processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) {
 }
+
--- source/wrapper/VstWrapper.h
+++ source/wrapper/VstWrapper.h
@@ -1,6 +1,12 @@
+#ifndef __VstWrapper_h__
+#define __VstWrapper_h__
+
 #include "audioeffectx.h"
 
 #define NUM_PARAMS 0
+#define NUM_INPUTS 2
+#define NUM_OUTPUTS 2
+#define UNIQUE_ID 'VstW'
 
 class VstWrapper : public AudioEffectX {
 public:
@@ -9,3 +15,5 @@ public:
 
   void processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames);
 };
+
+#endif
