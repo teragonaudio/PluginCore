0e45a396f97b0cadaf792b0ea4ff5f680ef296e0
Nik Reiman <teragonaudio@mac.com>
2010-02-18 16:56:15 +0100

    Added gtest support to project

---
+++ test/interface/buffer/AudioBufferTest.cpp
@@ -0,0 +1,10 @@
+#include "interface/buffer/AudioBuffer.h"
+#include "gtest/gtest.h"
+
+namespace teragon {
+namespace plugincore {
+  TEST(AudioBufferTest, testNothing) {
+    EXPECT_EQ(0, 1);
+  }
+}
+}
