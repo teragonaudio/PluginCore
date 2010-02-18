51fc96177f8d8f2e180b6d7ab07551c88029bcec
Nik Reiman <teragonaudio@mac.com>
2010-02-18 16:57:48 +0100

    Added test case stub for AudioBufferSet

---
+++ test/interface/buffer/AudioBufferSetTest.cpp
@@ -0,0 +1,10 @@
+#include "interface/buffer/AudioBufferSet.h"
+#include "gtest/gtest.h"
+
+namespace teragon {
+namespace plugincore {
+  TEST(AudioBufferSetTest, testNothing) {
+    EXPECT_EQ(0, 1);
+  }
+}
+}
