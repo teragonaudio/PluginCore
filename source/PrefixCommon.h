18bc5721f848cfc6b7350f8722d3f59501bbb719
Nik Reiman <teragonaudio@mac.com>
2010-03-30 14:44:01 +0200

    Added common preprocessor definition file which can be used for both mac/windows

---
+++ source/PrefixCommon.h
@@ -0,0 +1,15 @@
+#include <string>
+
+// Definitions common to all PluginCore API
+const std::string kPluginFactoryFunctionName = "createPluginInstance";
+
+// Load plugin definitions from the project
+#include "PluginDefinition.h"
+
+#ifndef PLUGIN_NAME
+#error PLUGIN_NAME must be defined in your project's PluginDefinition.h file
+#endif
+
+#ifndef PLUGIN_MANUFACTURER
+#error PLUGIN_MANUFACTURER must be defined in your project's PluginDefinition.h file
+#endif
