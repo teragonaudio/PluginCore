00ecc5c5a1da418b615b4b454b9d8423141af423
Nik Reiman <teragonaudio@mac.com>
2010-03-30 13:59:16 +0200

    Added some explanatory text in the comments, defined symbols for plugin name and manufacturer

---
+++ PluginDefinition.h
@@ -0,0 +1,10 @@
+/*
+ * Example PluginDefinition.h File
+ *
+ * Your plugin needs to also have a file named PluginDefinition.h located in the same folder
+ * as your IDE's project file.  You must define the same symbols listed below, which tell
+ * the various plugin wrappers where to find the actual library which your core file is located.
+ */
+
+#define PLUGIN_NAME "YourPluginName"
+#define PLUGIN_MANUFACTURER "Your Company Name"
