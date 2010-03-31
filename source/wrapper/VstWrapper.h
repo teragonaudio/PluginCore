885734ad3b31f5f0550560b1e7f0ef3cb712ae77
Nik Reiman <teragonaudio@mac.com>
2010-03-31 17:01:02 +0200

    Wrapper now is processing audio correctly, as it seems that one must write to the host's expected output buffers, not just fix the pointer addresses.  Also added hooks to get/set parameters.

--- source/wrapper/VstWrapper.h
+++ source/wrapper/VstWrapper.h
@@ -11,12 +11,20 @@
 
 namespace teragon {
 namespace plugincore {
+  // TODO: Should support user-defined channel counts.  For now, stereo is fine.
+  const int kNumChannels = 2;
+
   class VstWrapper : public AudioEffectX {
   public:
     VstWrapper(Plugin *plugin, audioMasterCallback audioMaster);
     ~VstWrapper();
 
     void processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames);
+
+	  float getParameter(VstInt32 index);
+	  void getParameterDisplay(VstInt32 index, char* text);
+	  void getParameterName(VstInt32 index, char* text);
+    void setParameter(VstInt32 index, float value);
 
   private:
     Plugin *plugin;
