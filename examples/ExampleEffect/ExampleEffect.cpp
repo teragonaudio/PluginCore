0c23bf07396dbc8f8d2f2ffccf4fa78d82472475
Nik Reiman <teragonaudio@mac.com>
2010-03-31 17:03:50 +0200

    ExampleEffect now implements a gain plugin. What fun!

--- examples/ExampleEffect/ExampleEffect.cpp
+++ examples/ExampleEffect/ExampleEffect.cpp
@@ -11,6 +11,10 @@
 #include "ExampleEffect.h"
 #endif
 
+#ifndef __ParameterFloat_h__
+#include "plugincore/parameter/ParameterFloat.h"
+#endif
+
 extern "C" {
   teragon::plugincore::Plugin* createPluginInstance() {
     return new ExampleEffect();
@@ -18,18 +22,25 @@ extern "C" {
 };
 
 ExampleEffect::ExampleEffect() : teragon::plugincore::Plugin(), parameters() {
+  initialize();
 }
 
 ExampleEffect::~ExampleEffect() {
 }
 
 void ExampleEffect::initialize() {
+  parameters.addParameter(new teragon::plugincore::ParameterFloat("Gain"));
 }
 
 void ExampleEffect::process(const teragon::plugincore::AudioBufferSet& inputs, teragon::plugincore::AudioBufferSet& outputs) {
+  teragon::plugincore::PluginParameter* gainParameter = parameters.getParameter("Gain");
+  float gain = static_cast<float>(gainParameter->getValue());
+
   for(int channel = 0; channel < inputs.getNumChannels(); ++channel) {
     for(int frame = 0; frame < inputs.getSize(); ++frame) {
-      outputs.setSample(channel, frame, inputs.getSample(channel, frame));
+      teragon::plugincore::Sample input = inputs.getSample(channel, frame);
+      teragon::plugincore::Sample output = input * gain;
+      outputs.setSample(channel, frame, output);
     }
   }
 }
