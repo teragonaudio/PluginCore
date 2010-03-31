ef48c63d28fbdd94f3e5ca44134bfede77ddf6b1
Nik Reiman <teragonaudio@mac.com>
2010-03-31 13:57:25 +0200

    Took examples out of a namespace, which is perhaps a more realistic use-case

--- examples/ExampleEffect/ExampleEffect.cpp
+++ examples/ExampleEffect/ExampleEffect.cpp
@@ -13,25 +13,23 @@
 
 extern "C" {
   teragon::plugincore::Plugin* createPluginInstance() {
-    return new teragon::ExampleEffect();
+    return new ExampleEffect();
   }
 };
 
-namespace teragon {
-  ExampleEffect::ExampleEffect() : teragon::plugincore::Plugin(), parameters() {
-  }
-  
-  ExampleEffect::~ExampleEffect() {
-  }
-  
-  void ExampleEffect::initialize() {
-  }
-  
-  void ExampleEffect::process(const teragon::plugincore::AudioBufferSet& inputs, teragon::plugincore::AudioBufferSet& outputs) {
-    for(int channel = 0; channel < inputs.getNumChannels(); ++channel) {
-      for(int frame = 0; frame < inputs.getSize(); ++frame) {
-        outputs.setSample(channel, frame, inputs.getSample(channel, frame));
-      }
+ExampleEffect::ExampleEffect() : teragon::plugincore::Plugin(), parameters() {
+}
+
+ExampleEffect::~ExampleEffect() {
+}
+
+void ExampleEffect::initialize() {
+}
+
+void ExampleEffect::process(const teragon::plugincore::AudioBufferSet& inputs, teragon::plugincore::AudioBufferSet& outputs) {
+  for(int channel = 0; channel < inputs.getNumChannels(); ++channel) {
+    for(int frame = 0; frame < inputs.getSize(); ++frame) {
+      outputs.setSample(channel, frame, inputs.getSample(channel, frame));
     }
   }
 }
--- examples/ExampleEffect/ExampleEffect.h
+++ examples/ExampleEffect/ExampleEffect.h
@@ -18,26 +18,24 @@
 #include "plugincore/parameter/ParameterSetMap.h"
 #endif
 
-namespace teragon {
-  class ExampleEffect : public teragon::plugincore::Plugin {
-  public:
-    ExampleEffect();
-    ~ExampleEffect();    
-    
-    void initialize();
-		void process(const teragon::plugincore::AudioBufferSet& inputs, teragon::plugincore::AudioBufferSet& outputs);
-		
-    const int getNumInputs() const { return 2; }
-    const int getNumOutputs() const { return 2; }
-    const unsigned long getPluginId() const { return 'exfx'; }
-    const unsigned long getPluginManufacturerId() const { return 'Tera'; }
-    const int getNumPrograms() const { return 0; }
+class ExampleEffect : public teragon::plugincore::Plugin {
+public:
+  ExampleEffect();
+  ~ExampleEffect();    
+  
+  void initialize();
+	void process(const teragon::plugincore::AudioBufferSet& inputs, teragon::plugincore::AudioBufferSet& outputs);
+	
+  const int getNumInputs() const { return 2; }
+  const int getNumOutputs() const { return 2; }
+  const unsigned long getPluginId() const { return 'exfx'; }
+  const unsigned long getPluginManufacturerId() const { return 'Tera'; }
+  const int getNumPrograms() const { return 0; }
 
-    const teragon::plugincore::PluginParameterSet& getParameterSet() const { return this->parameters; }
+  const teragon::plugincore::PluginParameterSet& getParameterSet() const { return this->parameters; }
 
-  private:
-    teragon::plugincore::ParameterSetMap parameters;
-  };
-}
+private:
+  teragon::plugincore::ParameterSetMap parameters;
+};
 
 #endif
